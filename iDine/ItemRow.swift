c9479b3effc933f47f2144a853c5a95642f0b349
Zach McArtor <zmcartor@gmail.com>
2021-02-18 12:52:34 -0700

    style restrictions in itemRow

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ItemRow: View {
+    let colors: [String: Color] = ["D": .purple, "G": .black,
+   "N": .red, "S": .blue, "V": .green]
     
     let item: MenuItem
     var body: some View {
@@ -22,6 +24,19 @@ struct ItemRow: View {
                 
                 Text("$\(item.price)")
                     .font(.subheadline)
+            }
+            
+            Spacer() // automatically takes up free space
+            
+            // id makes the strings value the identifier
+            ForEach(item.restrictions, id: \.self) { r in
+                Text(r)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[r, default: .green])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
